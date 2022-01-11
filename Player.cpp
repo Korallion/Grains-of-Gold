91ebd6960638bc25c95be7d0ce7f68c99e5edfd0
Korallion <karl.jakelski@gmail.com>
2022-01-12 10:20:22 +1300

    Added "sprinting" capabilities

--- Player.cpp
+++ Player.cpp
@@ -5,8 +5,8 @@ Player::Player()
     pos_x = 0;
     pos_y = 0;
 
-    vel_x = 1;
-    vel_y = 1;
+    vel_x = 3;
+    vel_y = 3;
 
     texture = new GameTexture();
 }
@@ -18,31 +18,36 @@ Player::~Player()
 
 void Player::renderPlayer( SDL_Renderer* renderer )
 {
-    texture->render( renderer, pos_x, pos_y );
+    texture->render( renderer, pos_x, pos_y, NULL, 1 );
 }
 
 void Player::positionUpdate( const Uint8* keyState )
 {
-    // Update position
-    // X direction first
-    // Check if only one of the x_directional keys is pressed
+    // Check if sprinting
+    int sprint = 1;
+
+    if (keyState[SDL_SCANCODE_LSHIFT]){
+        sprint = 2;
+    }
+
+    //Check x direction
     if (keyState[SDL_SCANCODE_A])
     {
-        pos_x -= vel_x;
+        pos_x -= vel_x * sprint;
     }
     else if (keyState[SDL_SCANCODE_D])
     {
-        pos_x += vel_x;
+        pos_x += vel_x * sprint;
     }
 
     // Y direction next
     if ( keyState[SDL_SCANCODE_S])
     {
-        pos_y += vel_y;
+        pos_y += vel_y * sprint;
     }
     else if (keyState[SDL_SCANCODE_W])
     {
-        pos_y -= vel_y;
+        pos_y -= vel_y * sprint;
     }
 
     
