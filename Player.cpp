b02be6c0a92e2b3b431c4a2dfc4c78979d975d0c
Korallion <karl.jakelski@gmail.com>
2022-01-13 21:33:52 +1300

    Slowed down diagonal player movement

--- Player.cpp
+++ Player.cpp
@@ -25,30 +25,34 @@ void Player::positionUpdate( const Uint8* keyState, float deltaTime)
 {
     // Check if sprinting
     float sprint = 1;
+    float diagonal = 1;
 
     if (keyState[SDL_SCANCODE_LSHIFT]){
         sprint = 1.8;
+    }
+    
+    // Fun check to see if player is moving diagonally, if so, we slow them down
+    if (((keyState[SDL_SCANCODE_A] || keyState[SDL_SCANCODE_D]) && keyState[SDL_SCANCODE_W]) || ((keyState[SDL_SCANCODE_A] || keyState[SDL_SCANCODE_D]) && keyState[SDL_SCANCODE_S])){
+        diagonal = 1/ 1.4;
     }
 
     //Check x direction
     if (keyState[SDL_SCANCODE_A])
     {
-        pos_x -= vel_x * sprint * deltaTime;
+        pos_x -= vel_x * sprint * diagonal * deltaTime;
     }
     else if (keyState[SDL_SCANCODE_D])
     {
-        pos_x += vel_x * sprint * deltaTime;
+        pos_x += vel_x * sprint * diagonal * deltaTime;
     }
 
     // Y direction next
     if ( keyState[SDL_SCANCODE_S])
     {
-        pos_y += vel_y * sprint * deltaTime;
+        pos_y += vel_y * sprint * diagonal * deltaTime;
     }
     else if (keyState[SDL_SCANCODE_W])
     {
-        pos_y -= vel_y * sprint * deltaTime;
-    }
-
-    
+        pos_y -= vel_y * sprint * diagonal * deltaTime;
+    }    
 }
